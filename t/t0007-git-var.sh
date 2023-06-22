426ed83040065217735a803090279ce1a90e07c3
brian m. carlson <bk2204@github.com>
2023-06-22 19:50:57 +0000

    var: add support for listing the shell
    On most Unix systems, finding a suitable shell is easy: one simply uses
    "sh" with an appropriate PATH value.  However, in many Windows
    environments, the shell is shipped alongside Git, and it may or may not
    be in PATH, even if Git is.
    
    In such an environment, it can be very helpful to query Git for the
    shell it's using, since other tools may want to use the same shell as
    well.  To help them out, let's add a variable, GIT_SHELL_PATH, that
    points to the location of the shell.
    
    On Unix, we know our shell must be executable to be functional, so
    assume that the distributor has correctly configured their environment,
    and use that as a basic test.  On Git for Windows, we know that our
    shell will be one of a few fixed values, all of which end in "sh" (such
    as "bash").  This seems like it might be a nice test on Unix as well,
    since it is customary for all shells to end in "sh", but there probably
    exist such systems that don't have such a configuration, so be careful
    here not to break them.
    
    Signed-off-by: brian m. carlson <bk2204@github.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0007-git-var.sh
+++ t/t0007-git-var.sh
@@ -147,6 +147,18 @@ test_expect_success 'get GIT_SEQUENCE_EDITOR with configuration and environment 
 	)
 '
 
+test_expect_success POSIXPERM 'GIT_SHELL_PATH points to a valid executable' '
+	git var GIT_SHELL_PATH >shell &&
+	test -x "$(cat shell)"
+'
+
+# We know in this environment that our shell will be one of a few fixed values
+# that all end in "sh".
+test_expect_success MINGW 'GIT_SHELL_PATH points to a suitable shell' '
+	git var GIT_SHELL_PATH >shell &&
+	grep "sh\$" shell
+'
+
 # For git var -l, we check only a representative variable;
 # testing the whole output would make our test too brittle with
 # respect to unrelated changes in the test suite's environment.
