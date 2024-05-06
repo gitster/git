7bcdadede954637f0e0c66571197ee1e72bed642
Junio C Hamano <gitster@pobox.com>
2024-05-06 13:41:12 -0700

    merge-fix/ps/refs-without-the-repository

--- sequencer.c
+++ sequencer.c
@@ -5041,7 +5041,7 @@ cleanup_head_ref:
 			}
 			msg = reflog_message(opts, "finish", "returning to %s",
 				head_ref.buf);
-			if (refs_create_symref(get_main_ref_store(the_repository), "HEAD", head_ref.buf, msg)) {
+			if (refs_update_symref(get_main_ref_store(the_repository), "HEAD", head_ref.buf, msg)) {
 				res = error(_("could not update HEAD to %s"),
 					head_ref.buf);
 				goto cleanup_head_ref;
