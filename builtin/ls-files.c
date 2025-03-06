4b106ff5d39df48d8b611e3fd89c90c0886c196d
Junio C Hamano <gitster@pobox.com>
2025-03-06 11:32:23 -0800

    merge-fix/ua/some-builtins-wo-the-repository

--- builtin/ls-files.c
+++ builtin/ls-files.c
@@ -233,7 +233,8 @@ static void show_submodule(struct repository *superproject,
 {
 	struct repository subrepo;
 
-	if (repo_submodule_init(&subrepo, superproject, path, null_oid(the_hash_algo)))
+	if (repo_submodule_init(&subrepo, superproject, path,
+				null_oid(superproject->hash_algo)))
 		return;
 
 	if (repo_read_index(&subrepo) < 0)
