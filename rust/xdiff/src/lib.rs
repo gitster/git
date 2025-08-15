4e388da5fa0989da115a4096295086e86f8548ad
Ezekiel Newren <ezekielnewren@gmail.com>
2025-08-15 01:22:49 +0000

    xdiff: implement a white space iterator in Rust
    Xdiff has traditionally implemented the logic for iterating over
    whitespace in every location that needed to do so. Create a consolidated
    iterator in Rust that we can call from each location. Write Rust unit
    tests to ensure the correctness of the Rust whitespace iterator and the
    chunked_iter_equal() function.
    
    Signed-off-by: Ezekiel Newren <ezekielnewren@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- rust/xdiff/src/lib.rs
+++ rust/xdiff/src/lib.rs
@@ -1,3 +1,13 @@
+pub mod xutils;
+
+pub const XDF_IGNORE_WHITESPACE: u64 = 1 << 1;
+pub const XDF_IGNORE_WHITESPACE_CHANGE: u64 = 1 << 2;
+pub const XDF_IGNORE_WHITESPACE_AT_EOL: u64 = 1 << 3;
+pub const XDF_IGNORE_CR_AT_EOL: u64 = 1 << 4;
+pub const XDF_WHITESPACE_FLAGS: u64 = XDF_IGNORE_WHITESPACE |
+    XDF_IGNORE_WHITESPACE_CHANGE |
+    XDF_IGNORE_WHITESPACE_AT_EOL |
+    XDF_IGNORE_CR_AT_EOL;
 
 
 #[no_mangle]
