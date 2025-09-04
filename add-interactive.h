32abd3da48cd67e2f133e4a44ef11971b4433eb2
Patrick Steinhardt <ps@pks.im>
2025-09-04 16:27:42 +0200

    add-patch: split out `struct interactive_options`
    The `struct add_p_opt` is reused both by our the infra for "git add -p"
    and "git add -i". Users of `run_add_i()` for example are expected to
    pass `struct add_p_opt`. This is somewhat confusing and raises the
    question which options apply to what part of the stack.
    
    But things are even more confusing than that: while callers are expected
    to pass in `struct add_p_opt`, these options ultimately get used to
    initialize a `struct add_i_state` that is used by both subsystems. So we
    are basically going full circle here.
    
    Refactor the code and split out a new `struct interactive_options` that
    hosts common options used by both. These options are then applied to a
    `struct interactive_config` that hosts common configuration.
    
    This refactoring doesn't yet fully detangle the two subsystems from one
    another, as we still end up calling `init_add_i_state()` in the "git add
    -p" subsystem. This will be fixed in a subsequent commit.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- add-interactive.h
+++ add-interactive.h
@@ -2,34 +2,20 @@
 #define ADD_INTERACTIVE_H
 
 #include "add-patch.h"
-#include "color.h"
 
 struct pathspec;
 struct repository;
 
 struct add_i_state {
 	struct repository *r;
-	int use_color;
-	char header_color[COLOR_MAXLEN];
-	char help_color[COLOR_MAXLEN];
-	char prompt_color[COLOR_MAXLEN];
-	char error_color[COLOR_MAXLEN];
-	char reset_color[COLOR_MAXLEN];
-	char fraginfo_color[COLOR_MAXLEN];
-	char context_color[COLOR_MAXLEN];
-	char file_old_color[COLOR_MAXLEN];
-	char file_new_color[COLOR_MAXLEN];
-
-	int use_single_key;
-	char *interactive_diff_filter, *interactive_diff_algorithm;
-	int context, interhunkcontext;
+	struct interactive_config cfg;
 };
 
 void init_add_i_state(struct add_i_state *s, struct repository *r,
-		      struct add_p_opt *add_p_opt);
+		      struct interactive_options *opts);
 void clear_add_i_state(struct add_i_state *s);
 
 int run_add_i(struct repository *r, const struct pathspec *ps,
-	      struct add_p_opt *add_p_opt);
+	      struct interactive_options *opts);
 
 #endif
