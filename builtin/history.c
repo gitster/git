62764fd8a12b6a9fd620424cbe8aa9d0adc0317d
Patrick Steinhardt <ps@pks.im>
2025-09-04 16:27:38 +0200

    builtin/history: introduce subcommands to manage interrupted rewrites
    Introduce subcommands to manage the sequencer state for git-history(1).
    These aren't really useful yet, but will become useful in subsequent
    commits where we will introduce git-history(1) subcommands that actually
    edit history.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/history.c
+++ builtin/history.c
@@ -1,20 +1,128 @@
 #include "builtin.h"
+#include "branch.h"
 #include "gettext.h"
 #include "parse-options.h"
+#include "sequencer.h"
 
-int cmd_history(int argc,
-		const char **argv,
-		const char *prefix,
-		struct repository *repo UNUSED)
+static int cmd_history_abort(int argc,
+			     const char **argv,
+			     const char *prefix,
+			     struct repository *repo)
 {
 	const char * const usage[] = {
-		N_("git history [<options>]"),
+		N_("git history abort"),
 		NULL,
 	};
 	struct option options[] = {
 		OPT_END(),
 	};
+	struct replay_opts opts = REPLAY_OPTS_INIT;
+	int ret;
 
 	argc = parse_options(argc, argv, prefix, options, usage, 0);
-	return 0;
+	if (argc) {
+		ret = error(_("command does not take arguments"));
+		goto out;
+	}
+
+	opts.action = REPLAY_HISTORY_EDIT;
+	ret = sequencer_rollback(repo, &opts);
+	if (ret)
+		goto out;
+
+	ret = 0;
+
+out:
+	replay_opts_release(&opts);
+	return ret;
+}
+
+static int cmd_history_continue(int argc,
+				const char **argv,
+				const char *prefix,
+				struct repository *repo)
+{
+	const char * const usage[] = {
+		N_("git history continue"),
+		NULL,
+	};
+	struct option options[] = {
+		OPT_END(),
+	};
+	struct replay_opts opts = REPLAY_OPTS_INIT;
+	int ret;
+
+	argc = parse_options(argc, argv, prefix, options, usage, 0);
+	if (argc) {
+		ret = error(_("command does not take arguments"));
+		goto out;
+	}
+
+	opts.action = REPLAY_HISTORY_EDIT;
+	ret = sequencer_continue(repo, &opts);
+	if (ret)
+		goto out;
+
+	ret = 0;
+
+out:
+	replay_opts_release(&opts);
+	return ret;
+}
+
+static int cmd_history_quit(int argc,
+			    const char **argv,
+			    const char *prefix,
+			    struct repository *repo)
+{
+	const char * const usage[] = {
+		N_("git history quit"),
+		NULL,
+	};
+	struct option options[] = {
+		OPT_END(),
+	};
+	struct replay_opts opts = REPLAY_OPTS_INIT;
+	int ret;
+
+	argc = parse_options(argc, argv, prefix, options, usage, 0);
+	if (argc) {
+		ret = error(_("command does not take arguments"));
+		goto out;
+	}
+
+	opts.action = REPLAY_HISTORY_EDIT;
+	ret = sequencer_remove_state(repo, &opts);
+	if (ret)
+		goto out;
+	remove_branch_state(repo, 0);
+
+	ret = 0;
+
+out:
+	replay_opts_release(&opts);
+	return ret;
+}
+
+int cmd_history(int argc,
+		const char **argv,
+		const char *prefix,
+		struct repository *repo)
+{
+	const char * const usage[] = {
+		N_("git history abort"),
+		N_("git history continue"),
+		N_("git history quit"),
+		NULL,
+	};
+	parse_opt_subcommand_fn *fn = NULL;
+	struct option options[] = {
+		OPT_SUBCOMMAND("abort", &fn, cmd_history_abort),
+		OPT_SUBCOMMAND("continue", &fn, cmd_history_continue),
+		OPT_SUBCOMMAND("quit", &fn, cmd_history_quit),
+		OPT_END(),
+	};
+
+	argc = parse_options(argc, argv, prefix, options, usage, 0);
+	return fn(argc, argv, prefix, repo);
 }
--- t/t3450-history.sh
+++ t/t3450-history.sh
@@ -6,7 +6,37 @@ test_description='tests for git-history command'
 
 test_expect_success 'refuses to do anything without subcommand' '
 	test_must_fail git history 2>err &&
-	test_grep foo err
+	test_grep "need a subcommand" err
+'
+
+test_expect_success 'abort complains about arguments' '
+	test_must_fail git history abort foo 2>err &&
+	test_grep "command does not take arguments" err
+'
+
+test_expect_success 'abort complains when no history edit is active' '
+	test_must_fail git history abort 2>err &&
+	test_grep "no history edit in progress" err
+'
+
+test_expect_success 'continue complains about arguments' '
+	test_must_fail git history continue foo 2>err &&
+	test_grep "command does not take arguments" err
+'
+
+test_expect_success 'continue complains when no history edit is active' '
+	test_must_fail git history continue 2>err &&
+	test_grep "no history edit in progress" err
+'
+
+test_expect_success 'quit complains about arguments' '
+	test_must_fail git history quit foo 2>err &&
+	test_grep "command does not take arguments" err
+'
+
+test_expect_success 'quit does not complain when no history edit is active' '
+	git history quit 2>err &&
+	test_must_be_empty err
 '
 
 test_done
