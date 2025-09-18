0273a06775df5368acd6a9385fa818f8cc3dbb39
Karthik Nayak <karthik.188@gmail.com>
2025-09-18 10:11:44 +0200

    reftable: check for trailing newline in 'tables.list'
    In the reftable format, the 'tables.list' file contains a newline
    separated list of tables. While we parse this file, we do not check or
    care about trailing newlines. Tighten the parser in `parse_names()` to
    return an appropriate error if there is no trailing newline.
    
    This requires modification to `parse_names()` to accept a third argument
    which will hold the error value.
    
    Signed-off-by: Karthik Nayak <karthik.188@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- reftable/basics.h
+++ reftable/basics.h
@@ -167,10 +167,11 @@ void free_names(char **a);
 
 /*
  * Parse a newline separated list of names. `size` is the length of the buffer,
- * without terminating '\0'. Empty names are discarded. Returns a `NULL`
- * pointer when allocations fail.
+ * without terminating '\0'. Empty names are discarded.
+ *
+ * Errors are assigned to the `err` variable.
  */
-char **parse_names(char *buf, int size);
+char **parse_names(char *buf, int size, int *err);
 
 /* compares two NULL-terminated arrays of strings. */
 int names_equal(const char **a, const char **b);
