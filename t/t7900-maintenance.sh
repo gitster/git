226f66ae94d878d6adc9dad345aa006746b808db
Patrick Steinhardt <ps@pks.im>
2026-02-20 11:15:05 +0100

    t: fix races caused by background maintenance
    Many Git commands spawn git-maintenance(1) to optimize the repository in
    the background. By default, performing the maintenance is for most of
    the part asynchronous: we fork the executable and then continue with the
    rest of our business logic.
    
    This is working as expected for our users, but this behaviour is
    somewhat problematic for our test suite as this is inherently racy. We
    have many tests that verify the on-disk state of repositories, and those
    tests may easily race with our background maintenance. In a similar
    fashion, we may end up with processes that "leak" out of a current test
    case.
    
    Until now this tends to not be much of a problem. Our maintenance uses
    git-gc(1) by default, which knows to bail out in case there aren't
    either too many packfiles or too many loose objects. So even if other
    data structures would need to be optimized, we won't do so unless the
    object database also needs optimizations.
    
    This is about to change though, as a subsequent commit will switch to
    the "geometric" maintenance strategy as a default. The consequence is
    that we will run required optimizations even if the object database is
    well-optimized. And this uncovers races between our test suite and
    background maintenance all over the place.
    
    Disabling maintenance outright in our test suite is not really an
    option, as it would result in significantly divergence from the "real
    world" and reduce our test coverage. But we've got an alternative up our
    sleeves: we can ensure that garbage collection runs synchronously by
    overriding the "maintenance.autoDetach" configuration.
    
    Of course that also diverges from the real world, as we now stop testing
    that background maintenance interacts in a benign way with normal Git
    commands. But on the other hand this ensures that the maintenance itself
    does not for example lead to data loss in a more reproducible way.
    
    Another concern is that this would make execution of the test suite much
    slower. But a quick benchmark on my machine demonstrates that this does
    not seem to be the case:
    
        Benchmark 1: meson test (revision = HEAD~)
          Time (mean ± σ):     131.182 s ±  1.293 s    [User: 853.737 s, System: 1160.479 s]
          Range (min … max):   130.001 s … 132.563 s    3 runs
    
        Benchmark 2: meson test (revision = HEAD)
          Time (mean ± σ):     129.554 s ±  0.507 s    [User: 849.040 s, System: 1152.664 s]
          Range (min … max):   129.000 s … 129.994 s    3 runs
    
        Summary
          meson test (revision = HEAD) ran
            1.01 ± 0.01 times faster than meson test (revision = HEAD~)
    
    Funny enough, it even seems as if this speeds up test execution ever so
    slightly, but that may just as well be noise.
    
    Introduce a new `GIT_TEST_MAINT_AUTO_DETACH` environment variable that
    allows us to override the auto-detach behaviour and set that varibale in
    our tests.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t7900-maintenance.sh
+++ t/t7900-maintenance.sh
@@ -6,6 +6,7 @@ test_description='git maintenance builtin'
 
 GIT_TEST_COMMIT_GRAPH=0
 GIT_TEST_MULTI_PACK_INDEX=0
+sane_unset GIT_TEST_MAINT_AUTO_DETACH
 
 test_lazy_prereq XMLLINT '
 	xmllint --version
