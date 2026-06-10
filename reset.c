e5ddbacb1c258e8ba8f020f99aa986f9d55a9b80
Patrick Steinhardt <ps@pks.im>
2026-06-10 10:52:26 +0200

    reset: introduce ability to skip updating HEAD
    In a subsequent commit we'll introduce a new caller to
    `reset_working_tree()` that really only wants to update the index and
    working tree, without updating any references. Introduce a new flag that
    makes the caller opt in to updating HEAD and adapt all callers to set
    that flag.
    
    Note that in a previous iteration we instead introduced a flag that made
    callers opt out of updating any references. This was somewhat awkward
    though because we already have the `UPDATE_ORIG_HEAD` flag, so the
    result was somewhat inconsistent.
    
    Suggested-by: Phillip Wood <phillip.wood123@gmail.com>
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- reset.c
+++ reset.c
@@ -92,6 +92,7 @@ int reset_working_tree(struct repository *r,
 	const char *switch_to_branch = opts->branch;
 	unsigned reset_hard = opts->flags & RESET_WORKING_TREE_HARD;
 	unsigned refs_only = opts->flags & RESET_WORKING_TREE_REFS_ONLY;
+	unsigned update_head = opts->flags & RESET_WORKING_TREE_UPDATE_HEAD;
 	unsigned update_orig_head = opts->flags & RESET_WORKING_TREE_UPDATE_ORIG_HEAD;
 	unsigned dry_run = opts->flags & RESET_WORKING_TREE_DRY_RUN;
 	struct object_id *head = NULL, head_oid;
@@ -129,7 +130,7 @@ int reset_working_tree(struct repository *r,
 		oid = &head_oid;
 
 	if (refs_only) {
-		if (!dry_run)
+		if (update_head)
 			return update_refs(r, opts, oid, head);
 		return 0;
 	}
@@ -197,7 +198,8 @@ int reset_working_tree(struct repository *r,
 		goto leave_reset_head;
 	}
 
-	if (oid != &head_oid || update_orig_head || switch_to_branch)
+	if (update_head &&
+	    (oid != &head_oid || update_orig_head || switch_to_branch))
 		ret = update_refs(r, opts, oid, head);
 
 leave_reset_head:
